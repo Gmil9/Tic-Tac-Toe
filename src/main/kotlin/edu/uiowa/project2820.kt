f3b7ba794e79a72c28398a9f7fd3f4fcf4718216
Garrett Miller <gmil2626@gmail.com>
2018-11-13 21:07:54 -0600

    Version 1 Structure

--- src/main/kotlin/edu/uiowa/project2820.kt
+++ src/main/kotlin/edu/uiowa/project2820.kt
@@ -4,8 +4,30 @@ package edu.uiowa
 // but you will also need to connect this project with your github.uiowa.edu
 // repository, as explained in the discussion section 9th November
 
+interface TheBoard{
+    fun createBoard()
+    fun checkBoard(board: Array<IntArray>): String
+    fun showBoard(board: Array<IntArray>)
+}
+
+
+class Board(val size: Int): TheBoard{
+
+    override fun createBoard() {
+        TODO("not implemented")
+    }
+
+    override fun checkBoard(board: Array<IntArray>): String {
+        TODO("not implemented")
+    }
+
+    override fun showBoard(board: Array<IntArray>) {
+        TODO("not implemented")
+    }
+}
+
 fun main(args: Array<String>) {
     // you can do some testing here, though unit testing needs to be
     // in the src/test/java directory
-    print("this is a test")
+    val b = Board(3)
 }
