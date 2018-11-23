438b3852c71bf59f682e1ef5cd7ca16d42d0e411
Garrett Miller <gmil2626@gmail.com>
2018-11-23 17:42:16 -0600

    javaFX first version

--- src/main/kotlin/edu/uiowa/project2820.kt
+++ src/main/kotlin/edu/uiowa/project2820.kt
@@ -3,9 +3,11 @@ import java.util.*
 
 import javafx.application.Application
 import javafx.application.Application.launch
+import javafx.application.Platform
 import javafx.event.ActionEvent
 import javafx.event.EventHandler
 import javafx.geometry.Insets
+import javafx.scene.Node
 import javafx.scene.Scene
 import javafx.scene.control.*
 import javafx.scene.layout.GridPane
@@ -16,13 +18,20 @@ import javafx.stage.Stage
 var current_stage: Stage? = null
 var grid = GridPane()
 var boardSize: Int = 3
+var buttons = arrayOf<Array<Button>>()
+var currentTurn = true
+var winner = 0
+var b = Board(boardSize)
+var board = arrayOf<Array<Int>>()
+var boardButtons = arrayOf<Array<Button>>()
 
 interface TheBoard{
     fun createBoard(): Array<Array<Int>>
-    fun checkBoard(board: Array<Array<Int>>): Int
-    fun showBoard(board: Array<Array<Int>>)
+    fun createBoardButtons(): Array<Array<Button>>
+    fun checkBoard(board: Array<Array<Int>>)
+    fun takeTurn(j: Int, i: Int)
+    fun showBoard()
 }
-
 
 class Board(val size: Int): TheBoard{
 
@@ -30,69 +39,112 @@ class Board(val size: Int): TheBoard{
     //then each row has an array of columns
     override fun createBoard(): Array<Array<Int>>{
         var rows = arrayOf<Array<Int>>()
+        var count = 0
         for(i in 1..size){
             var cols = arrayOf<Int>()
             for(j in 1..size){
-                val b = Button("")
-                b.setPrefSize((boardSize * 99).toDouble(), (boardSize * 99).toDouble())
-                grid.add(b, i, j)
                 cols += 0
+                count++
             }
             rows += cols
         }
+
         return rows
     }
 
-    //returns 1 if X wins, and -1 if O wins
-    override fun checkBoard(board: Array<Array<Int>>): Int {
+    override fun createBoardButtons(): Array<Array<Button>>{
+        var count = 0
+        for(i in 0..size-1){
+            var bcols = arrayOf<Button>()
+            for(j in 0..size-1){
+                val b = Button("X")
+                b.setPrefSize((size * 99).toDouble(), (size * 99).toDouble())
+                grid.add(b, i, j)
 
-        for(i in 0..2){
-            //checks for 3 in a row in each row
-            if(board[i][0] == board[i][1] && board[i][0] == board[i][2] && board[i][0] != 0){
-                if(board[i][0] == 1){
-                    return 1
-                }else{
-                    return -1
-                }
+                b.setOnAction { e -> takeTurn(i, j) }
+
+                bcols += b
+                count++
             }
-            //checks for 3 in a row in each column
-            if(board[0][i] == board[1][i] && board[0][i] == board[2][i] && board[0][i] != 0){
-                if(board[i][0] == 1){
-                    return 1
-                }else{
-                    return -1
-                }
-            }
-            //checks for 3 in a row diagonal TopLeft to BottomRight
-            if(board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != 0){
-                if(board[0][0] == 1){
-                    return 1
-                }else{
-                    return -1
-                }
-            }
-            //checks for 3 in a row diagonal TopRight to BottomLeft
-            if(board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != 0){
-                if(board[0][2] == 1){
-                    return 1
-                }else{
-                    return -1
-                }
+            buttons += bcols
+        }
+        return buttons
+    }
+
+    override fun takeTurn(j: Int, i: Int) {
+        if(board[i][j] != 0){
+            print("Square already taken, Choose again\n\n")
+            currentTurn = !currentTurn
+        }else{
+            if(currentTurn){
+                board[i][j] = 1
+            }else{
+                board[i][j] = -1
             }
         }
 
-        return 0
+        b.showBoard()
+        b.checkBoard(board)
+        if(winner == 1){
+            //Player 1 wins "X"
+            print("Player 1 Wins!")
+            println()
+            current_stage?.close()
+        }else if(winner == -1){
+            //Player 2 wins "O"
+            print("Player 2 Wins!")
+            println()
+            current_stage?.close()
+        }
+        currentTurn = !currentTurn
     }
 
-    override fun showBoard(board: Array<Array<Int>>) {
-        for (r in board){
-            for(c in r){
-                if(c == 0){
+    //returns 1 if X wins, and -1 if O wins
+    override fun checkBoard(board: Array<Array<Int>>) {
+
+        for(i in 0..size-1){
+            var count = 0
+            for(j in 0..size-2){
+                print("Board[i][j]: " + board[i][j] + " and Board[i][j+1]: " + board[i][j+1])
+                println()
+                if(board[i][j] == board[i][j+1] && board[i][j] != 0){
+                    count++
+                }
+            }
+            print("Count: " + count)
+            println()
+            if(count == size - 1){
+                winner = if(currentTurn) 1 else -1
+            }
+        }
+
+        for(i in 0..size-1){
+            var count = 0
+            for(j in 0..size-2){
+                print("Board[j][i]: " + board[j][i] + " and Board[j][i+1]: " + board[j+1][i])
+                println()
+                if(board[j][i] == board[j+1][i] && board[j][i] != 0){
+                    count++
+                }
+            }
+            print("Count: " + count)
+            println()
+            if(count == size - 1){
+                winner = if(currentTurn) 1 else -1
+            }
+        }
+
+    }
+
+    override fun showBoard() {
+        for (r in 0..size-1){
+            for(c in 0..size-1){
+                if(board[r][c] == 0){
                     print("[ ]")
-                }else if(c == 1){
-                    print(" X ")
-                }else{
-                    print(" O ")
+                }else if(board[r][c] == 1){
+                    print("X")
+                }else if(board[r][c] == -1){
+                    print("O")
                 }
             }
             println()
@@ -100,70 +152,35 @@ class Board(val size: Int): TheBoard{
     }
 }
 
-class MyForm: Application() {
+fun newGame(stage: Stage){
+    current_stage = stage
 
-    override fun start(primaryStage: Stage) {
-        current_stage = primaryStage
+    val entry = Scanner(System.`in`)
+    print("Choose a board size between 3 and 6")
+    boardSize = entry.nextInt()
 
-        val b = Board(boardSize)
-        var board = b.createBoard()
+    b = Board(boardSize)
+    board = b.createBoard()
+    boardButtons = b.createBoardButtons()
 
-        grid.gridLinesVisibleProperty().set(true)
+    grid.gridLinesVisibleProperty().set(true)
 
-
-        primaryStage.run {
-            scene = Scene(grid, (boardSize * 100).toDouble(),(boardSize * 100).toDouble())
-            show()
-        }
+    stage.run {
+        scene = Scene(grid, (boardSize * 100).toDouble(),(boardSize * 100).toDouble())
+        show()
     }
 }
 
+class MyForm: Application() {
+
+    override fun start(primaryStage: Stage) {
+        newGame(primaryStage)
+    }
+}
 
 fun main(args: Array<String>) {
     // you can do some testing here, though unit testing needs to be
     // in the src/test/java directory
-    val entry = Scanner(System.`in`)
-    print("Choose a board size between 3 and 6")
-    boardSize = entry.nextInt()
+
     launch(MyForm::class.java)
-
-//    val b = Board(3)
-//    var board = b.createBoard()
-//
-//    var currentTurn = true
-//    var winner = 0
-//
-//    while(winner == 0){
-//        val entry = Scanner(System.`in`)
-//        print("Enter a row")
-//        val r = entry.nextInt()
-//        println("$r\n")
-//        print("Enter a column")
-//        val c = entry.nextInt()
-//        println("$c\n")
-//
-//        if(board[r][c] != 0){
-//            print("Square already taken, Choose again\n\n")
-//            currentTurn = !currentTurn
-//        }else{
-//            if(currentTurn){
-//                board[r][c] = 1
-//            }else{
-//                board[r][c] = -1
-//            }
-//        }
-//
-//        b.showBoard(board)
-//        winner = b.checkBoard(board)
-//        currentTurn = !currentTurn
-//    }
-//
-//    if(winner == 1){
-//        //Player 1 wins "X"
-//        print("Player 1 Wins!")
-//    }else{
-//        //Player 2 wins "O"
-//        print("Player 2 Wins!")
-//    }
-
 }
