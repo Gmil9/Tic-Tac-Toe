34e79895f5ffa32206ae3bfd393114c47aed8980
Garrett Miller <gmil2626@gmail.com>
2018-11-23 21:15:20 -0600

    added menu scenes

--- src/main/kotlin/edu/uiowa/project2820.kt
+++ src/main/kotlin/edu/uiowa/project2820.kt
@@ -15,7 +15,8 @@ import javafx.scene.layout.HBox
 import javafx.scene.layout.VBox
 import javafx.stage.Stage
 
-var current_stage: Stage? = null
+var currentstage: Stage? = null
+var menuScene: Scene? = null
 var grid = GridPane()
 var boardSize: Int = 3
 var buttons = arrayOf<Array<Button>>()
@@ -24,6 +25,7 @@ var winner = 0
 var b = Board(boardSize)
 var board = arrayOf<Array<Int>>()
 var boardButtons = arrayOf<Array<Button>>()
+var sizeInput: TextField ?= null
 
 interface TheBoard{
     fun createBoard(): Array<Array<Int>>
@@ -89,12 +91,12 @@ class Board(val size: Int): TheBoard{
             //Player 1 wins "X"
             print("Player 1 Wins!")
             println()
-            current_stage?.close()
+            currentstage?.scene = menuScene
         }else if(winner == -1){
             //Player 2 wins "O"
             print("Player 2 Wins!")
             println()
-            current_stage?.close()
+            currentstage?.scene = menuScene
         }
         currentTurn = !currentTurn
     }
@@ -152,29 +154,67 @@ class Board(val size: Int): TheBoard{
     }
 }
 
-fun newGame(stage: Stage){
-    current_stage = stage
-
-    val entry = Scanner(System.`in`)
-    print("Choose a board size between 3 and 6")
-    boardSize = entry.nextInt()
-
+fun newGame(){
+    val t = sizeInput as TextField
+    boardSize = t.text.toInt()
     b = Board(boardSize)
     board = b.createBoard()
     boardButtons = b.createBoardButtons()
-
     grid.gridLinesVisibleProperty().set(true)
-
-    stage.run {
-        scene = Scene(grid, (boardSize * 100).toDouble(),(boardSize * 100).toDouble())
-        show()
-    }
 }
 
 class MyForm: Application() {
 
     override fun start(primaryStage: Stage) {
-        newGame(primaryStage)
+
+        var gameScene = Scene(grid, (boardSize * 100).toDouble(),(boardSize * 100).toDouble())
+
+        val vbox = VBox()
+        vbox.setPrefSize(300.0,200.0)
+
+        val vbox1 = VBox()
+        vbox1.setPrefSize(300.0,200.0)
+
+        menuScene = Scene(vbox, 300.0, 200.0)
+        var chooseScene = Scene(vbox1, 300.0, 200.0)
+
+        val hbox1 = HBox()
+        hbox1.setPadding(Insets(15.0, 12.0, 15.0, 12.0));
+        hbox1.setSpacing(15.0);   // to make it look nicer
+        vbox.children.add(hbox1)
+        val header = Label("Tic Tac Toe")
+        hbox1.children.add(header)
+        val hbox2 = HBox()
+        hbox2.setPadding(Insets(15.0, 12.0, 15.0, 12.0));
+        hbox2.setSpacing(15.0);   // to make it look nicer
+        vbox.children.add(hbox2)
+        val startb = Button("Start Game") // text in button
+        startb.setOnAction { e -> primaryStage.scene = chooseScene }    // handler for button click
+        hbox2.children.add(startb)             // add button to vbox
+        val exitb = Button("Exit Game") // text in button
+        exitb.setOnAction { e -> primaryStage.close()}    // handler for button click
+        hbox2.children.add(exitb)
+
+        val hbox3 = HBox()
+        //hbox3.setPadding(Insets(15.0, 12.0, 15.0, 12.0));
+        //hbox3.setSpacing(15.0);   // to make it look nicer
+        vbox1.children.add(hbox3)
+        val hbox4 = HBox()
+        //hbox4.setPadding(Insets(15.0, 12.0, 15.0, 12.0));
+        //hbox4.setSpacing(15.0);   // to make it look nicer
+        vbox1.children.add(hbox4)
+        val labelSize = Label("Choose board size: ")
+        hbox3.children.add(labelSize)
+        val t = TextField()
+        sizeInput = t
+        hbox3.children.add(t)
+        val startb2 = Button("Start") // text in button
+        startb2.setOnAction { e -> primaryStage.scene = gameScene; newGame() }    // handler for button click
+        hbox4.children.add(startb2)
+
+        currentstage = primaryStage
+        primaryStage.scene = menuScene
+        primaryStage.show()
     }
 }
 
