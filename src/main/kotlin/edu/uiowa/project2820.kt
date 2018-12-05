b82cc983e31b5bfaaded028774d18ec50ca2d6ed
Garrett Miller <gmil2626@gmail.com>
2018-12-04 21:02:53 -0600

    AI and Ultimate working, will refactor later...

--- src/main/kotlin/edu/uiowa/project2820.kt
+++ src/main/kotlin/edu/uiowa/project2820.kt
@@ -17,49 +17,59 @@ import javafx.stage.Stage
 import javafx.scene.control.Alert.AlertType
 import javafx.scene.control.Alert
 import sun.security.krb5.KrbException.errorMessage
+import java.awt.Checkbox
+import java.awt.CheckboxGroup
+import java.awt.SystemColor.text
 
-
-var currentstage: Stage? = null
+var mainScene: Scene? = null
+var currentScene: Scene? = null
+var currentStage: Stage? = null
 var menuScene: Scene? = null
-var grid = GridPane()
-var boardSize: Int = 3
-var currentTurn = true
-var players = 1
-var winner = 0
-var b = Board(boardSize)
-val ai = AI()
-var board = arrayOf<Array<Int>>()       //background for tracking winner
-var buttons = arrayOf<Array<Button>>() //visual representation of buttons
+
 val groupset1: Array<ToggleButton?> = arrayOfNulls(4) // for toggle buttons
 val tgroup1 = ToggleGroup()
 
+var mainBoard = Board(3,false)
+var currentBoard = Board(3,false) //mini game when playing ultimate
+
+var players = 1
+var isUlt = false
+
 interface TheBoard{
-    fun createBoard(): Array<Array<Int>>
-    fun createGrid(): Array<Array<Button>>
-    fun checkBoard(board: Array<Array<Int>>)
-    fun takeTurn(j: Int, i: Int, bs :Button)
+    fun showBoard()
+    fun createBoard()
+    fun createGrid()
 }
 
-class Board(val size: Int): TheBoard{
+class Board(val size: Int, var main:Boolean): TheBoard{
+
+    var grid = GridPane()
+    var board = arrayOf<Array<Int>>()       //background for tracking winner
+    var buttons = arrayOf<Array<Button>>() //visual representation of buttons
+    var winner = 0
+    var currentTurn = true
+    var tempButton: Button? = null
+    var tempi = 0
+    var tempj = 0
+    val ai = AI()
 
     //2D array, Array of rows each containing an array of columns
-    override fun createBoard(): Array<Array<Int>>{
-        var rows = arrayOf<Array<Int>>()
-        var count = 0
-        for(i in 1..size){
-            var cols = arrayOf<Int>()
-            for(j in 1..size){
-                cols += 0
-                count++
-            }
-            rows += cols
+    override fun createBoard(){
+        if(players == 1){
+            ai.createMap()
         }
-        return rows
+
+        for(i in 0..size-1){
+            var cols = arrayOf<Int>()
+            for(j in 0..size-1){
+                cols += 0
+            }
+            board += cols
+        }
     }
 
     //creates the representation of the board
-    override fun createGrid(): Array<Array<Button>>{
-        var count = 0
+    override fun createGrid(){
         for(i in 0..size-1){
             var bcols = arrayOf<Button>()
             for(j in 0..size-1){
@@ -67,163 +77,216 @@ class Board(val size: Int): TheBoard{
                 b.style = "-fx-font-size: 48px; -fx-text-fill: #b22222"
                 b.setPrefSize((size * 99).toDouble(), (size * 99).toDouble())
                 grid.add(b, j, i)
-                b.setOnAction { e -> takeTurn(i, j, b) } //takeTurn is called whenever a square is clicked
+
+                if(isUlt && main){
+                    b.setOnAction { e -> tempButton = b; tempi = i; tempj = j; gameController(false)}
+                }else if(isUlt && !main){
+                    b.setOnAction { e ->  tempButton = b; tempi = i; tempj = j; takeTurn(currentBoard)}
+                }else if(!isUlt && main){
+                    b.setOnAction { e -> tempButton = b; tempi = i; tempj = j; takeTurn(mainBoard)}
+                }
+
                 bcols += b
-                count++
             }
             buttons += bcols
         }
-        return buttons
+
     }
 
-    //this is activated on each button click
-    override fun takeTurn(j: Int, i: Int, bs: Button) {
-        if(board[j][i] != 0){
-            message("Square already taken, Choose again")
-            currentTurn = !currentTurn
+    override fun showBoard() {
+        for(i in board){
+            for(j in i){
+                print(j)
+            }
+            println()
+        }
+    }
+
+    inner class AI{
+
+        var open_squares: MutableList<Pair<Int, Int>> = mutableListOf()
+        var open_squaresList: MutableList<Int> = mutableListOf()
+
+        fun createMap(){
+            open_squares.clear()
+            for(i in 0..size-1){
+                for(j in 0..size-1){
+                    open_squares.add(Pair(i, j))
+                }
+            }
+        }
+
+        fun currentMap(){
+            open_squaresList.clear()
+            var count = 0
+            for(i in board){
+                for(j in i){
+                    if(j == 0){
+                        open_squaresList.add(count)
+                    }
+                    count++
+                }
+            }
+        }
+
+        fun chooseSquare(){
+            val tempRandom = open_squaresList.random()
+            val tempPair = open_squares.get(tempRandom)
+            val tempNode = grid.children.get(tempRandom) as Button
+
+            tempNode.text = "O"
+            board[tempPair.first][tempPair.second] = -1
+
+        }
+    }
+}
+
+//this is activated on each button click
+fun takeTurn(boardClass: Board) {
+    if(boardClass.board[boardClass.tempi][boardClass.tempj] != 0){
+        message("Square already taken, Choose again")
+        boardClass.currentTurn = !boardClass.currentTurn
+    }else{
+        if(boardClass.currentTurn){
+            boardClass.board[boardClass.tempi][boardClass.tempj] = 1
+            boardClass.tempButton?.text = "X"
         }else{
-            if(currentTurn){
-                board[j][i] = 1
-                bs.text = "X"
+            boardClass.board[boardClass.tempi][boardClass.tempj] = -1
+            boardClass.tempButton?.text = "O"
+        }
+    }
+
+    if(isUlt){
+        checkBoard(currentBoard)
+        checkminiWinner()
+        checkBoard(mainBoard)
+        checkmainWinner()
+    }else{
+        checkBoard(boardClass)
+        checkmainWinner()
+    }
+
+    boardClass.currentTurn = !boardClass.currentTurn
+
+    if(players == 1 && boardClass.currentTurn == false && boardClass.winner == 0){
+        boardClass.ai.currentMap()
+        if(!boardClass.ai.open_squaresList.isEmpty()){
+            boardClass.ai.chooseSquare()
+
+            if(isUlt){
+                checkBoard(currentBoard)
+                checkminiWinner()
+                checkBoard(mainBoard)
+                checkmainWinner()
             }else{
-                board[j][i] = -1
-                bs.text = "O"
+                checkBoard(boardClass)
+                checkmainWinner()
             }
-        }
 
-        b.checkBoard(board)
-        checkWinner()
-
-        if(players == 1 && currentTurn == false && winner == 0){
-            ai.chooseSquare()
+            boardClass.currentTurn = !boardClass.currentTurn
         }
     }
 
-    //returns 1 if X wins, and -1 if O wins
-    override fun checkBoard(board: Array<Array<Int>>) {
-        var count = 0
+}
 
-        //checks each row
-        for(i in 0..size-1){
-            count = 0
-            for(j in 0..size-2){
-                if(board[i][j] == board[i][j+1] && board[i][j] != 0){
-                    count++
-                }
-            }
-            if(count == size - 1){
-                winner = if(currentTurn) 1 else -1
-            }
-        }
+//returns 1 if X wins, and -1 if O wins
+fun checkBoard(boardClass: Board) {
+    var count = 0
 
-        //checks each column
-        for(i in 0..size-1){
-            count = 0
-            for(j in 0..size-2){
-                if(board[j][i] == board[j+1][i] && board[j][i] != 0){
-                    count++
-                }
-            }
-            if(count == size - 1){
-                winner = if(currentTurn) 1 else -1
-            }
-        }
-
-        //checks diagonally from top left to bottom right
+    //checks each row
+    for(i in 0..boardClass.size-1){
         count = 0
-        for(i in 0..size-2){
-            if(board[i][i] == board[i+1][i+1] && board[i][i] != 0){
+        for(j in 0..boardClass.size-2){
+            if(boardClass.board[i][j] == boardClass.board[i][j+1] && boardClass.board[i][j] != 0){
                 count++
             }
-            if(count == size - 1){
-                winner = if(currentTurn) 1 else -1
-            }
         }
+        if(count == boardClass.size - 1){
+            boardClass.winner = if(boardClass.currentTurn) 1 else -1
+        }
+    }
 
-        //checks diagonal from top right to bottom left
+    //checks each column
+    for(i in 0..boardClass.size-1){
         count = 0
-        for(i in 0..size-2){
-            if(board[i][size-1-i] == board[i+1][size-2-i] && board[i][size-1-i] != 0){
+        for(j in 0..boardClass.size-2){
+            if(boardClass.board[j][i] == boardClass.board[j+1][i] && boardClass.board[j][i] != 0){
                 count++
             }
-            if(count == size - 1){
-                winner = if(currentTurn) 1 else -1
+        }
+        if(count == boardClass.size - 1){
+            boardClass.winner = if(boardClass.currentTurn) 1 else -1
+        }
+    }
+
+    //checks diagonally from top left to bottom right
+    count = 0
+    for(i in 0..boardClass.size-2){
+        if(boardClass.board[i][i] == boardClass.board[i+1][i+1] && boardClass.board[i][i] != 0){
+            count++
+        }
+        if(count == boardClass.size - 1){
+            boardClass.winner = if(boardClass.currentTurn) 1 else -1
+        }
+    }
+
+    //checks diagonal from top right to bottom left
+    count = 0
+    for(i in 0..boardClass.size-2){
+        if(boardClass.board[i][boardClass.size-1-i] == boardClass.board[i+1][boardClass.size-2-i] && boardClass.board[i][boardClass.size-1-i] != 0){
+            count++
+        }
+        if(count == boardClass.size - 1){
+            boardClass.winner = if(boardClass.currentTurn) 1 else -1
+        }
+    }
+
+
+    //checks if board is full with no winner
+    count = 0
+    for(i in 0..boardClass.size - 1){
+        for(j in 0..boardClass.size - 1){
+            if(boardClass.board[i][j] != 0 && boardClass.winner == 0){
+                count++
             }
         }
+    }
 
-        //checks if board is full with no winner
-        count = 0
-        for(i in 0..size - 1){
-            for(j in 0..size - 1){
-                if(board[i][j] != 0 && winner == 0){
-                    count++
-                }
-            }
-        }
-
-        if(count == size * size){
+    if(count == boardClass.size * boardClass.size){
+        if(boardClass.main){
             message("No Winner")
-            currentstage?.scene = menuScene
+            currentStage?.scene = menuScene
+        }else{
+            message("No Winner")
+            boardClass.winner = 0
+            currentStage?.scene = mainScene
         }
     }
 }
 
-class AI{
-
-    var open_squares: MutableList<Pair<Int, Int>> = mutableListOf()
-    var open_squaresList: MutableList<Int> = mutableListOf()
-
-    fun createMap(){
-        open_squares.clear()
-        for(i in 0..boardSize-1){
-            for(j in 0..boardSize-1){
-                open_squares.add(Pair(i, j))
-            }
-        }
-    }
-
-    fun currentMap(){
-        open_squaresList.clear()
-        var count = 0
-        for(i in 0..boardSize-1){
-            for(j in 0..boardSize-1){
-                if(board[i][j] == 0){
-                    open_squaresList.add(count)
-                }
-                count++
-            }
-        }
-    }
-
-    fun chooseSquare(){
-        ai.currentMap()
-        val tempRandom = open_squaresList.random()
-        val tempPair = open_squares.get(tempRandom)
-        val tempNode = grid.children.get(tempRandom) as Button
-
-        tempNode.text = "O"
-        board[tempPair.first][tempPair.second] = -1
-
-        b.checkBoard(board)
-        checkWinner()
+fun checkminiWinner(){
+    if(currentBoard.winner == 1){
+        mainBoard.tempButton?.text = "X"
+        mainBoard.board[mainBoard.tempi][mainBoard.tempj] = 1
+        currentStage?.scene = mainScene
+    }else if(currentBoard.winner == -1){
+        mainBoard.tempButton?.text = "O"
+        mainBoard.board[mainBoard.tempi][mainBoard.tempj] = -1
+        currentStage?.scene = mainScene
     }
 }
-
-fun checkWinner(){
-    if(winner == 1){
-        //Player 1 wins "X"
+fun checkmainWinner(){
+    if(mainBoard.winner == 1){
         message("Player 1 Wins!")
-        currentstage?.scene = menuScene
-    }else if(winner == -1){
-        //Player 2/Computer wins "O"
+        currentStage?.scene = menuScene
+    }else if(mainBoard.winner == -1){
         if(players == 1){
             message("The Computer Wins!")
         }else{
             message("Player 2 Wins!")
         }
-        currentstage?.scene = menuScene
+        currentStage?.scene = menuScene
     }
-    currentTurn = !currentTurn
 }
 
 //this creates a pop up message when called
@@ -235,41 +298,46 @@ fun message(s: String){
     alert.showAndWait()
 }
 
-//resets the board for a new game
-fun newGame(){
-    winner = 0
-    if(grid.children.size != 0) grid.children.remove(0, boardSize * boardSize)
-    if(grid.children.size > 0){
-        grid.children.remove(0,1)
-    }
-    currentTurn = true
+fun gameController(isMain:Boolean){
+    var boardSize = 3
     when(tgroup1.selectedToggle){
         groupset1[0] -> boardSize = 3
         groupset1[1] -> boardSize = 4
         groupset1[2] -> boardSize = 5
         groupset1[3] -> boardSize = 6
     }
-    b = Board(boardSize)
-    board = b.createBoard()
-    buttons = b.createGrid()
-    grid.gridLinesVisibleProperty().set(true)
+
+    if(isMain){
+        mainBoard = Board(boardSize, isMain)
+        mainBoard.createBoard()
+        mainBoard.createGrid()
+
+        mainScene = Scene(mainBoard.grid, (boardSize * 210).toDouble(),(boardSize * 210)
+                .toDouble())
+        currentStage?.scene = mainScene
+    }else{
+        currentBoard = Board(3, isMain)
+        currentBoard.createBoard()
+        currentBoard.createGrid()
+
+        currentScene = Scene(currentBoard.grid, 310.toDouble(), 310.toDouble())
+        currentStage?.scene = currentScene
+    }
+
 }
 
 class MyForm: Application() {
 
     override fun start(primaryStage: Stage) {
 
-        var gameScene = Scene(grid, (boardSize * 210).toDouble(),(boardSize * 210)
-                .toDouble())
-
         val vbox = VBox()
-        vbox.setPrefSize(500.0,200.0)
+        vbox.setPrefSize(500.0,250.0)
 
         val vbox1 = VBox()
-        vbox1.setPrefSize(300.0,150.0)
+        vbox1.setPrefSize(300.0,175.0)
 
-        menuScene = Scene(vbox, 300.0, 200.0)
-        var chooseScene = Scene(vbox1, 300.0, 150.0)
+        menuScene = Scene(vbox, 300.0, 250.0)
+        var chooseScene = Scene(vbox1, 300.0, 175.0)
 
         val hbox1 = HBox()
         hbox1.setPadding(Insets(20.0, 20.0, 15.0, 65.0));
@@ -285,8 +353,9 @@ class MyForm: Application() {
         hbox2.setSpacing(15.0);
         vbox.children.add(hbox2)
 
+        val cb = CheckBox()
         val startb = Button("Start Game")
-        startb.setOnAction { e -> primaryStage.scene = chooseScene; newGame() }
+        startb.setOnAction { e -> primaryStage.scene = chooseScene; isUlt = cb.isSelected}
         hbox2.children.add(startb)
 
         val exitb = Button("Exit Game")
@@ -323,6 +392,14 @@ class MyForm: Application() {
         hbox3b.children.add(r4)
         groupset1[3] = r4
 
+        val hbox3c = HBox()
+        hbox3c.setPadding(Insets(5.0, 15.0, 15.0, 110.0));
+        hbox3c.setSpacing(15.0);
+        vbox.children.add(hbox3c)
+
+        cb.text = "Ultimate?"
+        hbox3c.children.add(cb)
+
         val hbox4 = HBox()
         hbox4.setPadding(Insets(15.0, 20.0, 15.0, 40.0));
         hbox4.setSpacing(15.0);
@@ -338,13 +415,22 @@ class MyForm: Application() {
         vbox1.children.add(hbox5)
 
         val startb2 = Button("1 Player")
-        startb2.setOnAction { e -> primaryStage.scene = gameScene; players = 1; ai.createMap() }
+        startb2.setOnAction { e -> gameController(true); players = 1}
         hbox5.children.add(startb2)
         val startb3 = Button("2 Players")
-        startb3.setOnAction { e -> primaryStage.scene = gameScene; players = 2 }
+        startb3.setOnAction { e -> gameController(true); players = 2}
         hbox5.children.add(startb3)
 
-        currentstage = primaryStage
+        val hbox6 = HBox()
+        hbox6.setPadding(Insets(10.0, 30.0, 10.0, 65.0));
+        hbox6.setSpacing(15.0);
+        vbox1.children.add(hbox6)
+
+        val back = Button("Back")
+        back.setOnAction { e -> primaryStage.scene = menuScene}
+        hbox6.children.add(back)
+
+        currentStage = primaryStage
         primaryStage.scene = menuScene
         primaryStage.show()
     }
